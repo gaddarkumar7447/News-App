d2e667da17a2d01de989e0f61aed75366fdcf482
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-09-07 01:21:00 +0530

    All fragments are add

---
+++ app/src/main/java/com/example/newsapp/Home.java
@@ -0,0 +1,19 @@
+package com.example.newsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Home extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, null);
+        return view;
+    }
+}
